821d378fca10f2f8be3e7516b630ea4b2628494c
blyzzrdz <blz@avalanche.local>
2019-09-24 18:21:16 -0700

    Added initial skeleton for to script and design ideas.

---
+++ to.py
@@ -0,0 +1,51 @@
+
+
+# initialize 
+# maybe do another script.
+
+
+
+def transport():
+    # read file
+
+    # error - no file found
+
+    # find a match
+
+    # cd into current directory. - hide it from terminal
+    # error - return match not found
+
+
+def add():
+    # check if file exists.
+
+    # create file if not.
+
+
+
+
+def edit():
+    # simply open the file in vim.
+    # if file doesn't exist - output error.
+
+
+def remove():
+
+
+
+
+def checkindex():
+    # return 0
+    # return index path
+
+
+
+
+def control():
+
+
+
+
+
+if __name__ == '__main__':
+    #  argparser ? maybe
