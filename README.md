652d09a2c9ca5eef3f3194969ca66f69c2c061f5
blyzzrdz <blz@avalanche.local>
2019-09-24 22:15:59 -0700

    Fill out with skeleton for add function, build out a few necessary commands.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # where_to
 Where to is a personal index tool used to change file paths quickly based on an index. 
+
+
--- to.py
+++ to.py
@@ -1,27 +1,69 @@
+#!/usr/bin/env python
 
+import os
+import sys
 
 # initialize 
 # maybe do another script.
 
 
+def get_index():
+    """
+    returns the index path established by the $HOME
+    variable.
+    #TODO: look for a more effective / consistent / portable approach
+    to this. may require a stateful(?) version of keeping the variable alive.
 
-def transport():
-    # read file
+    RTRN --
+    Returns a string of the path.
 
-    # error - no file found
+    ARGS --
+    n/a
+    """
+    home = os.getenv("HOME")
+    home += "/.whereto"
+    return home
+
+
+def transport(key):
+    index = get_index()
+
+    results = []
 
     # find a match
+    f = open(index, 'r')
+
+    for line in f:
+        if key in line:
+            results.append(line)
+
+
+    res_count = len(results)
+    if res_count == 0:
+        print("There were no results available.")
+        return 0
+    elif res_count == 1:
+        # GO
+    elif res_count > 2 and res <= 9:
+        # PRINT OPTIONS
+        # GATHER INPUT
+        # GO
+    else:
+        print("Too vague of a keyword - Clean the index or use a more specific keyword.")
+        # TOO MANY OPTIONS ERROR
 
     # cd into current directory. - hide it from terminal
-    # error - return match not found
+
 
 
 def add():
-    # check if file exists.
+    index = get_index()
 
-    # create file if not.
+    f = open(index, 'a+')
+    f.write(entry)
 
-
+    cwd = os.getcwd()
+    f.close()
 
 
 def edit():
@@ -30,14 +72,7 @@ def edit():
 
 
 def remove():
-
-
-
-
-def checkindex():
-    # return 0
-    # return index path
-
+    # Remove an argument in the python file.
 
 
 
@@ -48,4 +83,20 @@ def control():
 
 
 if __name__ == '__main__':
-    #  argparser ? maybe
+
+    for arg in sys.argv:
+        exe = []
+
+        # if arg == '-a':
+            # add
+
+        # if arg == '-e':
+            # edit
+
+        # if arg == '-x':
+            # remove
+
+        # if arg is right then transport
+
+
+
